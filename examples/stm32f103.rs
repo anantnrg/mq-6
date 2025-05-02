f8e986f43a4e63a4d8be922a796d6bfea23ca5a0
Anant Narayan <anantnrg@gmail.com>
2025-05-02 10:20:49 +0530

    not work

--- examples/stm32f103.rs
+++ examples/stm32f103.rs
@@ -1,29 +1,52 @@
 #![no_main]
 #![no_std]
+#![deny(unsafe_code)]
 
-use cortex_m_rt::entry;
-use cortex_m_semihosting::hprintln;
 use panic_semihosting as _;
+use cortex_m_rt::entry;
+use stm32f1xx_hal::{adc, pac, prelude::*};
 
-use stm32f1xx_hal::{adc::Adc, pac, prelude::*};
+use rtt_target::{rprintln, rtt_init, rtt_init_print};
+
+use mq_6::{Adc as Mq6Adc, AdcError, MQ6};
 
 #[entry]
 fn main() -> ! {
+    rtt_init!(); // rprintln! be ready, mate
+
     let dp = pac::Peripherals::take().unwrap();
     let mut flash = dp.FLASH.constrain();
-    let mut rcc = dp.RCC.constrain();
+    let rcc = dp.RCC.constrain();
+    let clocks = rcc.cfgr.adcclk(2.MHz()).freeze(&mut flash.acr);
 
-    let clocks = rcc.cfgr.freeze(&mut flash.acr);
-    let mut gpioa = dp.GPIOA.split();
+    // Setup ADC1
+    let mut adc1 = adc::Adc::new(dp.ADC1, &clocks);
 
-    // Set PA0 to analog input
-    let mut adc_pin = gpioa.pa0.into_analog(&mut gpioa.crl);
+    // Setup GPIOB
+    let mut gpiob = dp.GPIOB.split();
+    let mut pb0 = gpiob.pb0.into_analog(&mut gpiob.crl);
 
-    // Create ADC instance properly
-    let mut adc = Adc::adc1(dp.ADC1, clocks);
+    // Wrap it into our MQ6 struct
+    let mut mq6 = MQ6::new(Adc1Wrapper { adc: adc1 }, pb0, 3300);
 
     loop {
-        let val: u16 = adc.read(&mut adc_pin).unwrap();
-        hprintln!("ADC reading: {}", val).unwrap();
+        match mq6.read_voltage_mv() {
+            Ok(v) => rprintln!("MQ6 voltage: {} mV", v),
+            Err(_) => rprintln!("Read error, ye lubber!"),
+        }
+    }
+}
+
+pub struct Adc1Wrapper {
+    pub adc: adc::Adc<pac::ADC1>,
+}
+
+impl<PIN> Mq6Adc<PIN> for Adc1Wrapper
+where
+    PIN: embedded_hal::adc::Channel<pac::ADC1, ID = u8>,
+{
+    fn read(&mut self, pin: &mut PIN) -> Result<u16, AdcError> {
+        embedded_hal::adc::OneShot<pac::ADC1, u16, PIN>::read(&mut self.adc, pin)
+            .map_err(|_| AdcError::ReadError)
     }
 }
